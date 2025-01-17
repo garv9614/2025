23042b105b5e7b70a082e452306b2ad2b924469b
garv9614 <garv9614@gmail.com>
2025-01-17 05:52:18 +0000

    initial

---
+++ start-your-year-2025/typescript/challenges/part1.ts
@@ -0,0 +1,16 @@
+function plus(
+  x: number | string | undefined,
+  y: number | string | undefined,
+): number | string | undefined {
+  if (x === undefined || y === undefined) return 0;
+
+  if (typeof x === "number" && typeof y === "number") return x + y;
+
+  if (typeof x === "string" && typeof y === "string") return x + y;
+
+  return undefined;
+}
+
+console.log(plus(2025, 1));
+console.log(plus(undefined, 21));
+console.log(plus("StackUp", "Learn"));
